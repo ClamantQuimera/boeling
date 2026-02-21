d25b74358747a4b108555588049af5c0329e267f
Polo Rivera <absmungo369@outlook.com>
2026-02-20 23:58:31 -0600

    strike

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -3,12 +3,22 @@ defmodule Bowling do
 
   def score([[f, s, _] | []]), do: f + s
 
-  def score([[f, s] | rest]) do
-    if f + s == 10 do
-      [next_f | _] = hd(rest)
-      10 + next_f + score(rest)
-    else
-      f + s + score(rest)
-    end
+  def score([[10, nil] | rest]) do
+    [next_frame | rest_after] = rest
+    [first_roll | tail] = next_frame
+    second_roll =
+      case tail do
+        [s | _] when not is_nil(s) -> s
+        _ -> [s2 | _] = hd(rest_after)
+             s2
+      end
+    10 + first_roll + second_roll + score(rest)
   end
+
+  def score([[f, s] | rest]) when f + s == 10 do
+    [next_f | _] = hd(rest)
+    10 + next_f + score(rest)
+  end
+
+  def score([[f, s] | rest]), do: f + s + score(rest)
 end
