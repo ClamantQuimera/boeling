7bd4191a4fea3c16acbfc806e2d56847c17c68d6
Polo Rivera <absmungo369@outlook.com>
2026-02-20 23:41:25 -0600

    Oops, puro uno

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,5 +1,10 @@
 defmodule Bowling do
-  def score(game) do
-    0
+
+  def score([]), do: 0
+
+  def score([frame|rest]) do
+    first = hd(frame)
+    second = hd(tl(frame))
+    first + second + score(rest)
   end
 end
