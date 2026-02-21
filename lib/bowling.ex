5dc76a2bd3072350a8eeda44132f923e6bfa14a3
Polo Rivera <absmungo369@outlook.com>
2026-02-20 23:50:13 -0600

    spare

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,10 +1,14 @@
 defmodule Bowling do
-
   def score([]), do: 0
 
-  def score([frame|rest]) do
-    first = hd(frame)
-    second = hd(tl(frame))
-    first + second + score(rest)
+  def score([[f, s, _] | []]), do: f + s
+
+  def score([[f, s] | rest]) do
+    if f + s == 10 do
+      [next_f | _] = hd(rest)
+      10 + next_f + score(rest)
+    else
+      f + s + score(rest)
+    end
   end
 end
